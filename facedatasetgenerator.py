3643087f91e07afed42c6484f0dc4f2c0b14bc3e
melvinpaul123 <59479561+melvinpaul123@users.noreply.github.com>
2020-01-03 22:20:44 +0530

    Add files via upload

---
+++ facedatasetgenerator.py
@@ -0,0 +1,63 @@
+import cv2
+import numpy as np
+import sys
+import sqlite3
+import os
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+path = os.path.dirname(os.path.abspath(__file__))
+cap = cv2.VideoCapture(0)
+i=0
+
+def info(Id,Name,Age,Gen):
+    conn=sqlite3.connect(path+r'\databaseset.db')
+    cmd="SELECT * FROM People WHERE ID="+str(Id)
+    cursor=conn.execute(cmd)
+    isRecordExist=0
+    for row in cursor:
+        isRecordExist=1
+    if(isRecordExist==1):
+        cmd="UPDATE people SET NAME=' "+str(Name)+" ' WHERE ID="+str(Id)
+        cmd2="UPDATE people SET AGE=' "+str(Age)+" 'WHERE ID="+str(Id)
+        cmd3="UPDATE people SET GENDER=' "+str(Gen)+" ' WHERE ID="+str(Id)
+    else:
+        cmd="INSERT INTO People(ID,NAME,AGE,GENDER) Values("+str(Id)+",'"+str(Name)+"','"+str(Age)+"','"+str(Gen)+"')"
+        cmd2=""
+        cmd3=""
+    conn.execute(cmd)
+    conn.execute(cmd2)
+    conn.execute(cmd3)
+    conn.commit()
+    conn.close()
+        
+id=input('enter the Id= ')
+name=input('enter the Name= ')
+age=input('enter the Age= ')
+gender=input('enter the Gender(M/F/Other)= ')
+info(id,name,age,gender)
+
+while 1:
+    ret, img = cap.read()
+    cv2.resizeWindow('img', 600,500)
+    #cv2.line(img,(600,250),(0,250),(0,255,0),1)
+    #cv2.line(img,(250,0),(250,500),(0,255,0),1)
+    #cv2.circle(img, (300, 250), 5, (255, 255, 255), -1)
+    gray  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.5, minNeighbors = 5)
+
+    for (x,y,w,h) in faces:
+        i=i+1
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),5)
+        roi_gray  = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        cv2.imwrite("dataSet/face-"+str(id)+'.'+ str(i) + ".jpg", roi_gray)
+
+    cv2.imshow('img',img)
+   
+    k = cv2.waitKey(100) & 0xff
+    if k == 27:
+        break
+    if i>20:
+        cap.release()
+        cv2.destroyAllWindows()
+        break
---
+++ facedetectoridentify.py
@@ -0,0 +1,56 @@
+import cv2,os
+import numpy as np
+from PIL import Image
+import sys
+import sqlite3
+
+path = os.path.dirname(os.path.abspath(__file__))
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read(path+r'\trainer\trainer.yml')
+font =cv2.FONT_HERSHEY_SIMPLEX #Creates a font
+
+def getProfile(nbr_predicted):
+    conn=sqlite3.connect(path+r'\databaseset.db')
+    cmd="SELECT * FROM People WHERE ID="+str(nbr_predicted)
+    cursor=conn.execute(cmd)
+    profile=None
+    for row in cursor:
+        profile=row
+    conn.close()
+    return profile
+
+while 1:
+    ret, img = cap.read()
+    cv2.resizeWindow('img', 600,500)
+    #cv2.line(img,(600,250),(0,250),(0,255,0),1)
+    #cv2.line(img,(300,0),(300,500),(0,255,0),1)
+    #cv2.circle(img, (300, 250), 5, (255, 255, 255), -1)
+    gray  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.5, minNeighbors = 5)
+
+    for (x,y,w,h) in faces:
+        roi_gray  = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        nbr_predicted, conf = recognizer.predict(roi_gray)
+        profile=getProfile(nbr_predicted)
+        if(profile!=None):
+            if conf <=70:
+                cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),5)
+                cv2.putText(img,"ID: "+str(profile[0]), (x,y+h+20),font, 1, (255,255,255)) #Draw the text
+                cv2.putText(img,"Name:"+str(profile[1]),(x,y+h+45),font, 1,(255,255,255));
+                cv2.putText(img,"Age: "+str(profile[2]),(x,y+h+70),font, 1,(255,255,255));
+                cv2.putText(img,"Gender: "+str(profile[3]),(x,y+h+95),font, 1, (255,255,255));
+            else:
+                cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),5)#(b,g,r)
+                cv2.putText(img,"NOT IN THE SYSTEM", (x,y+h+20),font, 1, (255,255,255)) #Draw the text
+
+            
+    
+
+    cv2.imshow('img',img)
+   
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
