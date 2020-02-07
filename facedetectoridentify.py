770e6a4b932de8fb2b1fb742f06df79179334ada
melvinpaul123 <59479561+melvinpaul123@users.noreply.github.com>
2020-02-07 16:02:30 +0530

    Update facedetectoridentify.py

--- facedetectoridentify.py
+++ facedetectoridentify.py
@@ -1,18 +1,17 @@
 import cv2,os
-import numpy as np
 from PIL import Image
-import sys
 import sqlite3
+import sys
 
 path = os.path.dirname(os.path.abspath(__file__))
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 cap = cv2.VideoCapture(0)
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read(path+r'\trainer\trainer.yml')
 font =cv2.FONT_HERSHEY_SIMPLEX #Creates a font
 
 def getProfile(nbr_predicted):
-    conn=sqlite3.connect(path+r'\databaseset.db')
+    conn=sqlite3.connect(path+r'\facebase1.db')
     cmd="SELECT * FROM People WHERE ID="+str(nbr_predicted)
     cursor=conn.execute(cmd)
     profile=None
@@ -23,7 +22,7 @@ def getProfile(nbr_predicted):
 
 while 1:
     ret, img = cap.read()
-    cv2.resizeWindow('img', 600,500)
+    cv2.resizeWindow('img', 640,480)
     #cv2.line(img,(600,250),(0,250),(0,255,0),1)
     #cv2.line(img,(300,0),(300,500),(0,255,0),1)
     #cv2.circle(img, (300, 250), 5, (255, 255, 255), -1)
@@ -35,22 +34,24 @@ while 1:
         roi_color = img[y:y+h, x:x+w]
         nbr_predicted, conf = recognizer.predict(roi_gray)
         profile=getProfile(nbr_predicted)
+        print(conf)
         if(profile!=None):
-            if conf <=70:
+            if conf <=45:
                 cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),5)
-                cv2.putText(img,"ID: "+str(profile[0]), (x,y+h+20),font, 1, (255,255,255)) #Draw the text
-                cv2.putText(img,"Name:"+str(profile[1]),(x,y+h+45),font, 1,(255,255,255));
-                cv2.putText(img,"Age: "+str(profile[2]),(x,y+h+70),font, 1,(255,255,255));
-                cv2.putText(img,"Gender: "+str(profile[3]),(x,y+h+95),font, 1, (255,255,255));
+                cv2.putText(img,"ID: "+str(profile[0]), (x,y+h+20),font, 1, (255,255,255),2) #Draw the text
+                cv2.putText(img,"Name:"+str(profile[1]),(x,y+h+45),font, 1,(255,255,255),2);
+                cv2.putText(img,"Age: "+str(profile[2]),(x,y+h+70),font, 1,(255,255,255),2);
+                cv2.putText(img,"Gender: "+str(profile[3]),(x,y+h+95),font, 1, (255,255,255),2);
             else:
                 cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),5)#(b,g,r)
-                cv2.putText(img,"NOT IN THE SYSTEM", (x,y+h+20),font, 1, (255,255,255)) #Draw the text
+                cv2.putText(img,"NOT IN THE SYSTEM", (x,y+h+20),font, 1, (255,255,255),2) #Draw the text
 
             
-    
 
     cv2.imshow('img',img)
    
     k = cv2.waitKey(30) & 0xff
     if k == 27:
+        cap.release()
+        cv2.destroyAllWindows()
         break
