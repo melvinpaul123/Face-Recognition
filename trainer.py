f24ea31f1ebb8ed88bc5308d16cd687d44474b2e
melvinpaul123 <59479561+melvinpaul123@users.noreply.github.com>
2020-02-07 16:03:39 +0530

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,7 +1,9 @@
-import cv2,os
+import cv2
+import os
 import numpy as np
 from PIL import Image
 import sys
+import glob
 
 path = os.path.dirname(os.path.abspath(__file__))
 recognizer = cv2.face.LBPHFaceRecognizer_create()
@@ -10,27 +12,29 @@ dataPath = path+r'\dataSet'
 
 def get_images_and_labels(datapath):
      image_paths = [os.path.join(datapath, f) for f in os.listdir(datapath)]
+     print(image_paths)
      # images will contains face images
      images = []
      # labels will contains the label that is assigned to the image
      labels = []
      for image_path in image_paths:
-         # Read the image and convert to grayscale
-         image_pil = Image.open(image_path).convert('L')
-         # Convert the image format into numpy array
-         image = np.array(image_pil, 'uint8')
-         # Get the label of the image
-         nbr = int(os.path.split(image_path)[1].split(".")[0].replace("face-", ""))
-         #nbr=int(''.join(str(ord(c)) for c in nbr))
-         print(nbr)
-         # Detect the face in the image
-         faces = face_cascade.detectMultiScale(image)
-         # If face is detected, append the face to images and the label to labels
-         for (x, y, w, h) in faces:
-             images.append(image[y: y + h, x: x + w])
-             labels.append(nbr)
-             cv2.imshow("Adding faces to traning set...", image[y: y + h, x: x + w])
-             cv2.waitKey(10)
+         for img in glob.glob(image_path+r'/*.png'):
+             # Read the image and convert to grayscale
+             image_pil = Image.open(img).convert('L')
+             # Convert the image format into numpy array
+             image = np.array(image_pil, 'uint8')
+             # Get the label of the image
+             nbr = int(os.path.split(img)[1].split(".")[0].replace("face-", ""))
+             #nbr=int(''.join(str(ord(c)) for c in nbr))
+             print(nbr)
+             # Detect the face in the image
+             faces = face_cascade.detectMultiScale(image)
+             # If face is detected, append the face to images and the label to labels
+             for (x, y, w, h) in faces:
+                 images.append(image[y: y + h, x: x + w])
+                 labels.append(nbr)
+                 cv2.imshow("Adding faces to traning set...", image[y: y + h, x: x + w])
+                 cv2.waitKey(10)
      # return the images list and labels list
      return images, labels
 
